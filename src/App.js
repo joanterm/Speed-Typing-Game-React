ac76733e22f0f3023eecadf7032e44d87475ea3c
joanterm <jo_joanarg@yahoo.com>
2022-03-15 14:24:55 -0400

    Added calculateWordCount function

--- src/App.js
+++ src/App.js
@@ -4,11 +4,24 @@ import './App.css';
 function App() {
   const [inputValue, setInputValue] = useState("")
 
+  //HANDLES INPUT CHANGE
   const handleInputChange = (e) => {
     const {value} = e.target
     setInputValue(value)
   }
   console.log(inputValue)
+
+  //REMOVES WHITESPACE IF USER PRESSES SPACE ON KEYBOARD, SPLITS A STRING INTO AN ARRAY, RETURNS 0 IF THERE ARE NO WORDS TYPED IN
+  const calculateWordCount = (word) => {
+    if (word === "") {
+      return 0
+    } else {
+      return word.trim().split(" ").length
+    }
+    //OR:
+    // const wordArray = word.trim().split(" ")
+    // return wordArray.filter(eachWord => eachWord !== "").length
+  }
 
   return (
     <div>
@@ -18,7 +31,7 @@ function App() {
           onChange={handleInputChange}
       /> 
       <h4>Time remaining: </h4>
-      <button>Start Game</button>
+      <button onClick={() => console.log(calculateWordCount(inputValue))}>Start Game</button>
       <h1>Total amount of word you typed: </h1>
     </div>
   );
